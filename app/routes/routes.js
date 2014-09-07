e1c3288e2c3d834a9e9a8e3903b7acc33a568de7
Adam Barnhard <admbarn@gmail.com>
2014-09-07 18:04:21 -0500

    added initUpdate route

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -43,6 +43,7 @@ module.exports = function(app, express){
   app.use(security.bounce);
   app.delete('/logout', users.logout);
   app.get('/verify', users.verify);
+  app.put('/profile/init', users.initUpdate);
   app.get('/profile', users.profile);
   app.post('/profile/photos', users.addPhotos);
   app.put('/profile/photos/primary', users.setProfilePhoto);
