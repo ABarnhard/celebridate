2c5eb7fbc4413936bb8c2f8685cd7a3fdf68e14f
Chyld Medford <chyld.medford@gmail.com>
2014-11-15 01:38:31 +0000

    google oauth stopped working, removed until can sort out

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ var config = {};
 config.facebook = {
   clientId:'1513404225544139',
   clientSecret: process.env.FB_SECRET_CELEBRIDATE,
-  callbackUrl:'http://celebridate-vm.com:3000/auth/facebook/callback'
+  callbackUrl:'http://celebridate.adambarnhard.com/auth/facebook/callback'
 };
 
 config.google = {
-  clientId:'684183651325-0pc397hugr7iir31nkh6qj84p3obtj3u.apps.googleusercontent.com',
+  clientId:'408000227807-8jc469lkjfhn59tsummlu7tp6muqdu8r.apps.googleusercontent.com',
   clientSecret: process.env.GG_SECRET_CELEBRIDATE,
-  callbackUrl:'http://celebridate-vm.com:3000/auth/google/callback'
+  callbackUrl:'http://celebridate.adambarnhard.com/auth/google/callback'
 };
 
 module.exports = config;
